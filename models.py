db1e4d90dcd6c7d2a4f925b1d6df3b441f0849e6
Esp <demid2207@gmail.com>
2025-04-17 15:56:54 +0300

    Reworked pytests and functions

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, Integer, String, Float, DateTime
 
-DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/db"
+DATABASE_URL = "postgresql+asyncpg://user:password@db:5432/db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
